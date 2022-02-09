650e73bae3efbdf02e26bf90077487438f3f023c
abdulshakur <abdulshakur486@gmail.com>
2022-02-09 17:31:22 +0300

    accomplishments

---
+++ src/components/Accomplishments/Accomplishments.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { achievements } from "../../constants/constants";
+
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AccomplishmentsStyles";
+
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {achievements.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}+`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider />
+  </Section>
+);
+
+export default Acomplishments;
