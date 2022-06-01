c1f94cf24a034ee04e32ad47f3c453971e75ccbb
abdulshakur <abdulshakur486@gmail.com>
2022-06-01 16:50:28 +0300

    added space.

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 
 export const LeftSection = styled(motion.div)`
   width: 100%;
+
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 80%;
     display: flex;
@@ -10,11 +11,11 @@ export const LeftSection = styled(motion.div)`
 
     margin: 0 auto;
   }
+
   @media ${(props) => props.theme.breakpoints.md} {
     width: 100%;
     display: flex;
     flex-direction: column;
-
     margin: 0 auto;
   }
 `;
