f0e4a063245048d8988451939e3d25597c069b06
abdulshakur <abdulshakur486@gmail.com>
2022-06-02 17:00:41 +0300

    decreased time.

--- netlify/functions/prevent.js
+++ netlify/functions/prevent.js
@@ -12,7 +12,7 @@ function startKeepAlive() {
         .get(urls[i], { method: "GET" })
         .then((data) => console.log(data.config.url));
     }
-  }, 58 * 60 * 1000); // load every 58 minutes
+  }, 5000); // load every 58 minutes
 }
 
 startKeepAlive();
