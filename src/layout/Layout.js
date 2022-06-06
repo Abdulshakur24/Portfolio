76f6da456fbf4caed51dad82abcbe635fff8404d
abdulshakur <abdulshakur486@gmail.com>
2022-06-06 11:49:09 +0300

    added a resume link.

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, Footer } from "../components";
-import { Container } from "./LayoutStyles";
+import { Container, ResumeWrapper } from "./LayoutStyles";
+import { FiExternalLink } from "react-icons/fi";
+import { AnimatePresence } from "framer-motion";
+
+import resume from "../assets/ashakur-resume.pdf";
 
 export const Layout = ({ children }) => {
+  const [visited, setVisited] = useState(false);
+
   return (
     <Container>
       <Header />
-      <main>{children}</main>
+      <main>
+        {children}
+        <AnimatePresence>
+          {!visited && (
+            <ResumeWrapper
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              onClick={() => setVisited(() => true)}
+            >
+              <a rel="noreferrer" href={resume} target="_blank">
+                <FiExternalLink />
+                <h6>Resume</h6>
+              </a>
+            </ResumeWrapper>
+          )}
+        </AnimatePresence>
+      </main>
       <Footer />
     </Container>
   );
