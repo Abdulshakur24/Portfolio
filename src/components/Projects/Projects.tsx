a13f341bf380789e2346ee973d6b1a4767606e72
abdulshakur <abdulshakur486@gmail.com>
2022-06-18 16:01:16 +0300

    migrated to typescript

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,123 @@
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
+import { motion } from "framer-motion";
+
+function SlideInWhenVisible({ children }: { children: JSX.Element }) {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.5, delay: 0.25 }}
+      variants={{
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+function Projects(): JSX.Element {
+  return (
+    <Section id="projects">
+      <SectionDivider
+        initial={{ y: 0, x: -1000, opacity: 0 }}
+        animate={{ y: 0, x: 0, opacity: 1 }}
+        transition={{ delay: 0.3 }}
+        // divider="true"
+      />
+      <SectionTitle
+        initial={{ y: 0, x: -1000, opacity: 0 }}
+        animate={{ y: 0, x: 0, opacity: 1 }}
+        transition={{ delay: 0.15 }}
+      >
+        Personal Projects
+      </SectionTitle>
+      <GridContainer>
+        {projects.map(
+          ({ id, title, image, tags, description, visit, source }) => {
+            return (
+              <BlogCardComponent
+                key={id}
+                id={id}
+                title={title}
+                image={image}
+                tags={tags}
+                description={description}
+                visit={visit}
+                source={source}
+              />
+            );
+          }
+        )}
+      </GridContainer>
+    </Section>
+  );
+}
+
+function BlogCardComponent({
+  title,
+  image,
+  tags,
+  description,
+  visit,
+  source,
+}: {
+  title: string;
+  description: string;
+  image: string;
+  tags: string[];
+  source: string;
+  visit: string;
+  id: number;
+}): JSX.Element {
+  return (
+    <SlideInWhenVisible>
+      <BlogCard>
+        <Img src={image} alt={title} />
+        <TitleContent>
+          <HeaderThree>{title}</HeaderThree>
+          <Hr />
+        </TitleContent>
+        <CardInfo className="card-info">{description}</CardInfo>
+        <div>
+          <TitleContent>Stack:</TitleContent>
+          <TagList>
+            {tags.map((t, index) => {
+              return <Tag key={index}>{t}</Tag>;
+            })}
+          </TagList>
+        </div>
+        <UtilityList>
+          <ExternalLinks href={visit} target={"_blank"}>
+            Visit
+          </ExternalLinks>
+          <ExternalLinks href={source} target={"_blank"}>
+            Source
+          </ExternalLinks>
+        </UtilityList>
+      </BlogCard>
+    </SlideInWhenVisible>
+  );
+}
+
+export default Projects;
